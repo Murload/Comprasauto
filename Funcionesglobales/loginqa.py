134a01d539d777e3579e49b57c231fc78eb3a9f9
Murload7 <a.murcia@montechelo.online>
2023-01-13 09:09:17 -0500

    First commit

---
+++ Funcionesglobales/loginqa.py
@@ -0,0 +1,32 @@
+import time
+import unittest
+from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import TimeoutException
+from Funcionesglobales.funciselenium import Funciones_Globales
+
+
+class Login_qasoul(unittest.TestCase):
+    def __init__(self,driver):
+        self.driver=driver
+
+
+    def loginqa(self):
+        f = Funciones_Globales(self.driver)
+        f.Texto_Mixto("xpath", "//input[@id='mat-input-0']", "testautos_leo", 1)
+        f.Texto_Mixto("xpath", "//input[@id='mat-input-1']", "Diciembre123*", 1)
+        f.Click_Mixto("xpath", "//button[@type='submit']", 1)
+        sleep(7)
+
+    def menucompras(self):
+        f = Funciones_Globales(self.driver)
+        f.Click_Mixto("xpath", "(//div[contains(.,'Menú')])[3]", 1)
+        f.Click_Mixto("xpath", "//button[contains(.,'Módulos')]", 1)
+        
+        
+
---
+++ comprasmain.py
@@ -0,0 +1,33 @@
+import unittest
+from os import remove
+from selenium import webdriver
+from time import sleep
+from pyunitreport import HTMLTestRunner
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from Funcionesglobales.funciselenium import Funciones_Globales
+from Funcionesglobales.loginqa import Login_qasoul
+
+
+class Comprasauto(unittest.TestCase):
+
+    def setUp(self):
+        self.driver=webdriver.Chrome(executable_path="C:\Driver\chromedriver.exe")
+        funciones = Funciones_Globales(self.driver)
+        funciones.Navegar("http://qamios.groupcos.com/login", 1)
+        
+    def test_compras(self):
+        f = Funciones_Globales(self.driver)
+        login = Login_qasoul(self.driver)
+        login.loginqa()
+        login.menucompras()
+        
+
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
+
